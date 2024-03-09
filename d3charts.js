304fba6222591bfeebd971ba84e288cde790b035
Eric Sun <2397286134@qq.com>
2024-03-08 21:35:09 -0800

    Add all interactive interaction and put in the script

---
+++ d3charts.js
@@ -0,0 +1,147 @@
+const width = 920;
+const height = 500;
+const margin = { top: 20, right: 30, bottom: 30, left: 50 };
+
+const svg = d3.select("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+// 模拟数据  
+const data = [160, 176, 256, 306, 358, 565, 670, 800, 848, 911, 930, 936, 969, 1015, 1062, 774, 977, 643, 1002, 911].map((item, i) => {
+    return {
+        x: 2005 + i,
+        y: item
+    }
+})
+
+// X比例尺  
+const xScale = d3.scaleLinear()
+    .domain(d3.extent(data, d => d.x))
+    .range([0, width]);
+
+// Y比例尺  
+const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.y)])
+    .range([height, 0]);
+
+// 添加X轴  
+svg.append("g")
+    .attr("class", "x-axis")
+    .attr('stroke','#ddd')
+    .attr('font-size',12)
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(xScale).ticks(10).tickFormat(d => d));
+
+// 添加Y轴  
+svg.append("g")
+    .attr("class", "y-axis")
+    .attr('stroke','#ddd')
+    .attr('font-size', 12)
+    .call(d3.axisLeft(yScale));
+
+svg.select('x-axis').selectAll(".tick line") // 选择刻度线  
+    .attr("stroke", "#ddd"); // 设置颜色为  
+  
+svg.select('y-axis').selectAll(".domain") // 选择坐标轴的主线  
+    .attr("stroke", "#ddd"); // 设置颜色为
+
+svg.select('y-axis').selectAll('.domain').attr("stroke", "#ddd"); 
+
+// 添加网格线  
+svg.append("g")
+    .attr("class", "grid")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(xScale).ticks(10).tickSize(-height, 0, 0).tickFormat(""));
+
+svg.append("g")
+    .attr("class", "grid")
+    .call(d3.axisLeft(yScale).ticks(10).tickSize(-width, 0, 0).tickFormat(""));
+
+// 折线图  
+const line = d3.line()
+    .x(d => xScale(d.x))
+    .y(d => yScale(d.y));
+
+svg.append("path")
+    .datum(data)
+    .attr("class", "line")
+    .attr("d", line);
+
+// 工具提示  
+let tooltip = d3.select("body").select("#tooltip");
+// 折线的点  
+const dots = svg.selectAll(".dot")
+    .data(data)
+    .enter().append("circle")
+    .attr("class", "dot")
+    .attr("r", 5)
+    .attr("cx", d => xScale(d.x))
+    .attr("cy", d => yScale(d.y))
+    .on("mouseover", function (d) {
+        d3.select(this).classed("highlight", true);
+        showTooltip(d); // 显示工具提示  
+        addCrosshair(d);
+    })
+    .on("mouseout", function (d) {
+        d3.select(this).classed("highlight", false);
+        // hideTooltip(); // 隐藏工具提示  
+        // removeCrosshair();  
+    });
+
+// 添加十字星和高亮  
+let crosshairG = null;
+
+function addCrosshair(d) {
+    if (!crosshairG) {
+        crosshairG = svg.append("g").attr("class", "crosshair");
+    }
+
+    const x = xScale(d.x);
+    const y = yScale(d.y);
+
+    crosshairG.selectAll("*").remove();
+
+    crosshairG.append("line")
+        .attr("x1", 0)
+        .attr("y1", y)
+        .attr("x2", width)
+        .attr("y2", y);
+
+    crosshairG.append("line")
+        .attr("x1", x)
+        .attr("y1", 0)
+        .attr("x2", x)
+        .attr("y2", height);
+
+    crosshairG.append("circle")
+        .attr("cx", x)
+        .attr("cy", y)
+        .attr("r", 5);
+}
+
+function removeCrosshair() {
+    if (crosshairG) {
+        crosshairG.selectAll("*").remove();
+    }
+}
+
+// 显示工具提示  
+function showTooltip(d) {
+    console.log(d,d3.event)
+    tooltip.style("display", "block")
+        .style("left", `${d3.event.clientX - margin.left - (window.document.body.clientWidth / 2 - 500) + 100}px`)
+        .style("top", `${d3.event.clientY - 80}px`)
+        .html(`
+                    <div style="display:flex; flex-direction:column; align-items: flex-start;">
+                        <div><b>year:</b> ${d.x}</div>
+                        <div><b>客流量:</b> ${d.y * 10} thousand</div>
+                    </div>
+                `);
+}
+
+// 隐藏工具提示  
+function hideTooltip() {
+    tooltip.style("display", "none");
+}  
--- map.js
+++ map.js
@@ -62,6 +62,8 @@ window.map = map;
 map.addControl(new mapboxgl.NavigationControl());
 
 let animation = null;
+let routes = null;
+let stations = null;
 
 map.on('load', async function () {
 
@@ -69,6 +71,14 @@ map.on('load', async function () {
   // map.loadImage('https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',(error, image) => {
   //   map.addImage('default-marker', image);
   // })
+
+  let metro_line = await fetchSync('./data/metro_line.json')
+  console.log(metro_line)
+  let metro_station = await fetchSync('./data/metro_station.json')
+  
+  routes = deepCopy(metro_line);
+  console.log(routes)
+  stations = deepCopy(metro_station);
 
   map.addSource('source_metro_station', {
     'type': 'geojson',
@@ -80,6 +90,13 @@ map.on('load', async function () {
     'type': 'geojson',
     // 'data': './data/metro_line.json'
     data: { type: 'FeatureCollection', features: [] }
+  })
+
+  map.addSource('source_metro_line2', {
+    'type': 'geojson',
+    data:routes,
+    // 'data': './data/metro_line.json'
+    // data: { type: 'FeatureCollection', features: [] }
   })
 
   map.addSource('source_shanghai', {
@@ -94,6 +111,26 @@ map.on('load', async function () {
     source: 'source_metro_line',
     paint: {
       'line-color': ['get', 'color'],
+      'line-width': 4,
+      'line-opacity': 1
+    },
+    // filter: ['==', ['get', 'shortName'], '11号线']
+  });
+
+  map.addLayer({
+    id: 'layer_metro_line2',
+    type: 'line',
+    source: 'source_metro_line2',
+    layout: {
+      visibility: 'none',
+    },
+    paint: {
+      'line-color': [
+        'case',
+        ['boolean', ['feature-state', 'hover'], true],
+        ['get', 'color'],
+        '#aaa'
+      ],
       'line-width': 4,
       'line-opacity': 1
     },
@@ -128,113 +165,122 @@ map.on('load', async function () {
     console.log(e);
   })
 
-  let metro_line = await fetchSync('./data/metro_line.json')
-  console.log(metro_line)
-  let metro_station = await fetchSync('./data/metro_station.json')
-  console.log(metro_line)
-
-  const routes = deepCopy(metro_line);
-  const stations = deepCopy(metro_station);
-
   // 介绍顺序
-  let showNames = ['1号线', '2号线', '3号线', '4号线', '5号线', '6号线', '7号线', '8号线', '9号线', '10号线',
-    '11号线', '12号线', '13号线', '15号线', '16号线', '17号线', '18号线', '磁悬浮', '浦江线'];
+  let showNames = ['1号线', '2号线', '3号线', '4号线', '5号线', '6号线', '7号线', '8号线', '9号线', '10号线', '11号线', '12号线', '13号线', '15号线', '16号线', '17号线', '18号线', '磁悬浮']; // '浦江线'
   let pause = false;
   let name = '1号线'
   let mode = 'add'; // minus
   let count = 0;
   let lineFeature = {}, lineIndex = 0, coordinatesArr = [], preLineFeature = {}, preLineIndex = 0, preCoordinatesArr = [];
 
-  const sections = $('.section');
+  const sections = $('.section'); // ;
+  const sectionMaps = $('.section-map'); // $('.section-map');
   let currentSection = 0;
   let goingon = true;
   $(window).on('wheel', function (e) {
     console.log(e.originalEvent.deltaY, currentSection)
     // 防抖
-    if(!goingon) return;
+    if (!goingon) return;
     goingon = false;
-    setTimeout(()=>{
+    setTimeout(() => {
       goingon = true
-    },1200)
+    }, 1200)
+
 
     if (e.originalEvent.deltaY < 0) { // 向上滚动  
       if (currentSection === 0) {
-        window.parent.goPageOne()  // 首页上一屏
+        window.parent.goPagePre()  // 首页上一屏
       }
+
       if (currentSection > 0) {
         currentSection--;
         $('html, body').animate({
           scrollTop: sections.eq(currentSection).offset().top
         }, 500);
+        if (currentSection < sectionMaps.length) {
+          name = showNames[currentSection + 1]
+          mode = 'minus'
+          console.log(name, mode)
+          // lineFeature = routes.features.find(fea => fea.properties.shortName === name)
+          // lineIndex = routes.features.findIndex(fea => fea.properties.shortName === name)
+          // coordinatesArr = lineFeature.geometry.coordinates[0]
+          // count = lineFeature.geometry.coordinates[0].length;
+          // let currentIndex = 
+          metro_line = {
+            type: 'FeatureCollection',
+            features: routes.features.map((fea, index) => {
+              const { shortName } = fea.properties
 
-        name = showNames[currentSection + 1]
-        mode = 'minus'
-        console.log(name, mode)
-        // lineFeature = routes.features.find(fea => fea.properties.shortName === name)
-        // lineIndex = routes.features.findIndex(fea => fea.properties.shortName === name)
-        // coordinatesArr = lineFeature.geometry.coordinates[0]
-        // count = lineFeature.geometry.coordinates[0].length;
-        // let currentIndex = 
-        metro_line = {
-          type: 'FeatureCollection',
-          features: routes.features.map((fea, index) => {
-            const { shortName } = fea.properties
-
-            if (showNames.slice(0, currentSection + 1).indexOf(shortName) !== -1) {
-              return {
-                properties: { ...fea.properties },
-                geometry: {
-                  ...fea.geometry,
+              if (showNames.slice(0, currentSection + 1).indexOf(shortName) !== -1) {
+                return {
+                  properties: { ...fea.properties },
+                  geometry: {
+                    ...fea.geometry,
+                  }
+                }
+              } else {
+                return {
+                  properties: { ...fea.properties },
+                  geometry: {
+                    ...fea.geometry,
+                    coordinates: [[]]
+                  }
                 }
               }
-            } else {
-              return {
-                properties: { ...fea.properties },
-                geometry: {
-                  ...fea.geometry,
-                  coordinates: [[]]
-                }
-              }
-            }
 
-          })
-        }
+            })
+          }
 
-        metro_station = {
-          type: 'FeatureCollection',
-          features: stations.features.filter(fea => {
-            const { line } = fea.properties
-            return hasIntersection(line, showNames.slice(0, currentSection + 1))
-          })
+          metro_station = {
+            type: 'FeatureCollection',
+            features: stations.features.filter(fea => {
+              const { line } = fea.properties
+              return hasIntersection(line, showNames.slice(0, currentSection + 1))
+            })
+          }
         }
       }
     } else { // 向下滚动  
+      // if (currentSection == sections.length - 1){
+      //     window.parent.goPageNext()  // 下一屏
+      // }
       if (currentSection < sections.length - 1) {
         currentSection++;
         $('html, body').animate({
           scrollTop: sections.eq(currentSection).offset().top
         }, 500);
 
-        name = showNames[currentSection]
-        mode = 'add'
-        lineFeature = routes.features.find(fea => fea.properties.shortName === name)
-        lineIndex = routes.features.findIndex(fea => fea.properties.shortName === name)
-        coordinatesArr = lineFeature.geometry.coordinates[0]
-        count = 0
+        if (currentSection < sectionMaps.length) {
+          name = showNames[currentSection]
+          mode = 'add'
+          lineFeature = routes.features.find(fea => fea.properties.shortName === name)
+          lineIndex = routes.features.findIndex(fea => fea.properties.shortName === name)
+          coordinatesArr = lineFeature.geometry.coordinates[0]
+          count = 0
 
-        preLineFeature = routes.features.find(fea => fea.properties.shortName === showNames[currentSection - 1])
-        preLineIndex = routes.features.findIndex(fea => fea.properties.shortName === showNames[currentSection - 1])
-        preCoordinatesArr = preLineFeature.geometry.coordinates[0]
-        metro_line.features[preLineIndex].geometry.coordinates[0] = preCoordinatesArr;
+          preLineFeature = routes.features.find(fea => fea.properties.shortName === showNames[currentSection - 1])
+          preLineIndex = routes.features.findIndex(fea => fea.properties.shortName === showNames[currentSection - 1])
+          preCoordinatesArr = preLineFeature.geometry.coordinates[0]
+          metro_line.features[preLineIndex].geometry.coordinates[0] = preCoordinatesArr;
 
-        metro_station = {
-          type: 'FeatureCollection',
-          features: stations.features.filter(fea => {
-            const { line } = fea.properties
-            return hasIntersection(line, showNames.slice(0, currentSection))
-          })
+          metro_station = {
+            type: 'FeatureCollection',
+            features: stations.features.filter(fea => {
+              const { line } = fea.properties
+              return hasIntersection(line, showNames.slice(0, currentSection))
+            })
+          }
         }
       }
+    }
+    if (currentSection !== 19) {
+      map.setLayoutProperty('layer_metro_line2', 'visibility', 'none');
+    } else {
+      map.setLayoutProperty('layer_metro_line2', 'visibility', 'visible');
+      map.setFeatureState(
+        { source: 'source_metro_line2', id: 2 },
+        { hover: true },
+      );
     }
     // 阻止默认滚动行为  
     return false;
@@ -304,4 +350,303 @@ $('#zoomIn').click(() => {
 // 缩小
 $('#zoomOut').click(() => {
   map.zoomOut()
+})
+
+
+// render bar
+let riJunKeliu = [
+  {
+    name: 'Line 1',
+    kl1: 175,
+    kl2: 96,
+    color: '#e3032a'
+  },
+  {
+    name: 'Line 2',
+    kl1: 187,
+    kl2: 108,
+    color: '#93d508'
+  },
+  {
+    name: 'Line 3',
+    kl1: 62,
+    kl2: 40,
+    color: '#fbd602'
+  },
+  {
+    name: 'Line 4',
+    kl1: 98,
+    kl2: 43,
+    color: '#461d85'
+  },
+  {
+    name: 'Line 5',
+    kl1: 32,
+    kl2: 20,
+    color: '#944b9a'
+  },
+  {
+    name: 'Line 6',
+    kl1: 62,
+    kl2: 40,
+    color: '#e20265'
+  },
+  {
+    name: 'Line 7',
+    kl1: 62,
+    kl2: 40,
+    color: '#ec6e00'
+  },
+  {
+    name: 'Line 8',
+    kl1: 62,
+    kl2: 40,
+    color: '#0095d9'
+  },
+  {
+    name: 'Line 9',
+    kl1: 62,
+    kl2: 40,
+    color: '#86c9ec'
+  },
+  {
+    name: 'Line 10',
+    kl1: 62,
+    kl2: 40,
+    color: '#c6b0d4'
+  },
+  {
+    name: 'Line 11',
+    kl1: 62,
+    kl2: 40,
+    color: '#86192a'
+  },
+  {
+    name: 'Line 12',
+    kl1: 62,
+    kl2: 40,
+    color: "#00785f"
+  },
+  {
+    name: 'Line 13',
+    kl1: 62,
+    kl2: 40,
+    color: '#e799c0'
+  },
+  {
+    name: 'Line 14',
+    kl1: 62,
+    kl2: 40,
+    color: '#cab48f'
+  },
+  {
+    name: 'Line 15',
+    kl1: 62,
+    kl2: 40,
+    color: '#cab48f'
+  },
+  {
+    name: 'Line 16',
+    kl1: 39,
+    kl2: 23,
+    color: '#98d1c0'
+  },
+  {
+    name: 'Line 17',
+    kl1: 42,
+    kl2: 24,
+    color: '#bc7971'
+  },
+  {
+    name: 'Line 18',
+    kl1: 61,
+    kl2: 34,
+    color: '#c4984f'
+  },
+  {
+    name: '浦江线',
+    kl1: 11,
+    kl2: 7,
+    color: '#008B9A'
+  },
+]
+
+let htmlRj = ``
+riJunKeliu.forEach(element => {
+  const { name, kl1, kl2, color } = element
+  htmlRj += `<div class="item ${name === 'Line 2' ? 'active' : ''}">
+  <div class="sub1">
+    <label>${kl1}</label>
+    <div class="bar" style="width: ${kl1 / 200 * 100}%;"></div>
+  </div>
+  <div class="sub2" style="background:${color}">${name}</div>
+  <div class="sub3">
+    <div class="bar" style="width: ${kl2 / 200 * 100}%;"></div>
+    <label>${kl2}</label>
+  </div>
+</div>`
+})
+
+$('#chart1').html(htmlRj)
+
+// let hoveredPolygonId = null;
+// $('#chart1').on('click', '.item', function(){
+//   $(this).siblings().removeClass('active')
+//   $(this).addClass('active')
+//   $(this).siblings().find('label').removeClass('show')
+//   $(this).find('label').addClass('show')
+//   const lineName = $(this).find('.sub2').text()
+//   let shortName = ''
+//   if (lineName === '浦江线') {
+//     shortName = '蒲江线'
+//   } else {
+//     shortName = lineName.split(' ')[1] + '号线'
+//   }
+
+//   if (hoveredPolygonId !== null) {
+//     map.setFeatureState(
+//       { source: 'source_metro_line', id: hoveredPolygonId },
+//       { hover: false },
+//     );
+//   }
+//   hoveredPolygonId = routes.features.find(it=>it.properties.shortName === shortName)?.id;
+//   console.log(hoveredPolygonId);
+//   if (!hoveredPolygonId) return;
+//   map.setLayoutProperty('layer_metro_line2', 'visibility', 'visible');
+//   map.setFeatureState(
+//     { source: 'source_metro_line', id: hoveredPolygonId },
+//     { hover: true },
+//   );
+
+// })
+
+// $('#chart1').on('mouseover', '.item', function(){
+//   $(this).siblings().find('label').removeClass('show')
+//   $(this).siblings('.active').find('label').addClass('show')
+//   $(this).find('label').addClass('show')
+// })
+
+let stationsData = [
+  {
+    'weekday': { name: '世纪大道', lines: ['2号线', '4号线', '6号线', '9号线'], kl: 50 },
+    'holiday': { name: '人民广场', lines: ['1号线', '2号线', '8号线'], kl: 30 }
+  },
+  {
+    'weekday': { name: '人民广场', lines: ['1号线', '2号线', '8号线'], kl: 46 },
+    'holiday': { name: '世纪大道', lines: ['2号线', '4号线', '6号线', '9号线'], kl: 24 }
+  },
+  {
+    'weekday': { name: '徐家汇', lines: ['1号线', '9号线', '11号线'], kl: 36 },
+    'holiday': { name: '虹桥火车站', lines: ['2号线', '10号线', '17号线'], kl: 22 }
+  },
+  {
+    'weekday': { name: '虹桥火车站', lines: ['2号线', '10号线', '17号线'], kl: 33 },
+    'holiday': { name: '徐家汇', lines: ['1号线', '9号线', '11号线'], kl: 20 }
+  },
+  {
+    'weekday': { name: '汉中路', lines: ['1号线', '12号线', '13号线'], kl: 27 },
+    'holiday': { name: '南京东路', lines: ['2号线', '10号线'], kl: 19 }
+  },
+  {
+    'weekday': { name: '静安寺', lines: ['2号线', '7号线', '14号线'], kl: 27 },
+    'holiday': { name: '静安寺', lines: ['2号线', '7号线', '14号线'], kl: 15 }
+  },
+  {
+    'weekday': { name: '南京东路', lines: ['2号线', '10号线'], kl: 26 },
+    'holiday': { name: '上海火车站', lines: ['1号线', '3号线', '4号线'], kl: 13 }
+  },
+  {
+    'weekday': { name: '龙阳路', lines: ['2号线', '7号线', '16号线', '18号线'], kl: 23 },
+    'holiday': { name: '龙阳路', lines: ['2号线', '7号线', '16号线', '18号线'], kl: 13 }
+  },
+  {
+    'weekday': { name: '辛庄', lines: ['1号线', '5号线'], kl: 21 },
+    'holiday': { name: '陕西南路', lines: ['1号线', '10号线', '12号线'], kl: 13 }
+  },
+  {
+    'weekday': { name: '陕西南路', lines: ['1号线', '10号线', '12号线'], kl: 21 },
+    'holiday': { name: '汉中路', lines: ['1号线', '12号线', '13号线'], kl: 11 }
+  }
+]
+
+let htmlStas = ``
+stationsData.forEach((element, idx) => {
+  const { name: w_name, lines: w_lines, kl: w_kl } = element['weekday']
+  const { name: h_name, lines: h_lines, kl: h_kl } = element['holiday']
+  htmlStas += `<div class="item">
+  <div class="sub1" fid="${idx}" tp="weekday">
+    <div class="bar1">
+      <span>${w_kl}</span>
+      <div class="bar" style="width:${w_kl}px;"></div>
+    </div>
+    <div class="sub21">
+      ${w_lines.map(line => {
+    let color = riJunKeliu.find(it => it.name === `Line ${line.split('号线')[0]}`).color
+    return `<div class="sub2" style="background:${color}">Line ${line.split('号线')[0]}</div>`
+  }).join('')}
+    </div>
+    <label class="label">${w_name}</label>
+  </div>
+  <div class="sub-m"></div>
+  <div class="sub3" fid="${idx}" tp="holiday">
+    <label class="label">${h_name}</label>
+    <div class="sub21">
+      ${h_lines.map(line => {
+    let color = riJunKeliu.find(it => it.name === `Line ${line.split('号线')[0]}`).color
+    return `<div class="sub2" style="background:${color}">Line ${line.split('号线')[0]}</div>`
+  }).join('')}
+    </div>
+    <div class="bar1">
+      <div class="bar" style="width:${h_kl}px;"></div>
+      <span>${h_kl}</span>
+    </div>
+  </div>
+</div>`
+})
+htmlStas += `<div class="w_title">工作日日均客流最高10个地铁站</div><div class='h_title'>周末日均客流最高10个地铁站</div>`
+$('#chart2').html(htmlStas)
+
+// let hoveredPolygonId = null;
+$('#chart2').on('click', '.item >div', function () {
+  $(this).parents('.item').siblings().find('div').removeClass('active')
+  $(this).addClass('active')
+  // $(this).siblings().find('label').removeClass('show')
+  // $(this).find('label').addClass('show')
+  const fid = $(this).attr('fid')
+  const tp = $(this).attr('tp')
+  // const lines = Array.from(new Set((stationsData[+fid]['weekday'].lines).concat(stationsData[+fid]['holiday'].lines)))
+  const lines = stationsData[+fid][tp]['lines']
+
+  map.setLayoutProperty('layer_metro_line2', 'visibility', 'visible');
+
+  routes.features.forEach(fea => {
+    const { shortName } = fea.properties;
+    console.log(fea.id, typeof fea.id)
+    // let shortName_ = ''
+    // if (shortName === '浦江线') {
+    //   shortName_ = '蒲江线'
+    // } else {
+    //   shortName_ = 'Line ' + shortName.split('号线')[0]
+    // }
+
+    if (lines.includes(shortName)) {
+      map.setFeatureState(
+        { source: 'source_metro_line2', id: fea.id },
+        { hover: true },
+      );
+    } else {
+      map.setFeatureState(
+        { source: 'source_metro_line2', id: fea.id },
+        { hover: false },
+      );
+    }
+  })
+  console.log(lines);
+
+})
+
+$('#chart2').on('mouseover', '.item', function () {
+  $(this).siblings().find('label').removeClass('show')
+  // $(this).siblings('.active').find('label').addClass('show')
+  // $(this).find('label').addClass('show')
 })
